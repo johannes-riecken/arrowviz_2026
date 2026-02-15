8d0cad8322989f55ba220c29cae8ad2ed6cad29a
Johannes Riecken <johannes.riecken@gmail.com>
2026-02-15 11:32:46 +0100

    Fix test

--- tests/recognizers/test_shapes.py
+++ tests/recognizers/test_shapes.py
@@ -138,25 +138,6 @@ def test_recognizes_dashed_rectangle() -> None:
     )
 
 
-def test_recognizes_rounded_rectangle_within_circle() -> None:
-    fixture = Path("tests/data/06.png")
-
-    with fixture.open("rb") as image_file:
-        result = recognize_schematic(image_file)
-
-    assert result == Schematic(
-        shapes=(
-            Shape(
-                id="shape-0",
-                shape_type=ShapeType.CIRCLE,
-                child=Shape(
-                    id="shape-1",
-                    shape_type=ShapeType.ROUNDED,
-                ),
-            ),
-        )
-    )
-
 def test_recognizes_heart() -> None:
     fixture = Path("tests/data/08.png")
 
@@ -190,6 +171,22 @@ def test_recognizes_circle_with_incoming_arrow_from_left_border() -> None:
     assert edges == [("left-border", "shape-0")]
 
 
+def test_recognizes_circle_with_outgoing_arrow_to_right_border() -> None:
+    fixture = Path("tests/data/10.png")
+
+    with fixture.open("rb") as image_file:
+        result, edges = recognize_graph(image_file)
+
+    assert result == Schematic(
+        shapes=(
+            Shape(
+                id="shape-0",
+                shape_type=ShapeType.CIRCLE,
+            ),
+        )
+    )
+    assert edges == [("shape-0", "right-border")]
+
 def test_recognizes_arrow_from_left_border_to_rounded_rectangle_within_circle() -> None:
     fixture = Path("tests/data/11.png")
 
@@ -208,4 +205,4 @@ def test_recognizes_arrow_from_left_border_to_rounded_rectangle_within_circle() 
             ),
         )
     )
-    assert edges == [("left-border", "shape-0")]
+    assert edges == [("left-border", "shape-1")]
